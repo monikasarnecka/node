246a37bbac662a36da25a3a0119e757fe689c010
monikasarnecka <monika.sarnecka@interia.pl>
2022-02-26 12:09:03 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const port = process.env.PORT || 3030;
+const app = express();
+const path = require('path')
+const functions = require("./functions");
+
+
+app.set("view engine","hbs")
+
+app.use("/assets", express.static(path.join(__dirname, "./assets")));
+app.use("/js", express.static(path.join(__dirname, "./js")));
+
+app.get ("/", function (req, res){
+    res.render("index", {
+        title:"Tytuł strony",
+        subtitle:"Podtytuł na alk",
+        dodawanie: functions.add(1,2)
+    });
+});
+
+app.get("/about", function (req, res){
+    res.render("about", {
+        title:"Tytuł strony",
+        subtitle:"Podtytuł na alk",
+        dodawanie: functions.add(1,2)
+    });
+});
+app.get("/kontakt", function (req,res){
+    res.send("Kontakt ze mną");
+});
+
+app.listen(port, (err) =>{
+    if (err) {
+        return console.log(`Coś poszło nie tak: ${err}`);
+    }
+    console.log("serwer działa na porcie", port);
+})
+
+
---
+++ functions.js
@@ -0,0 +1,8 @@
+const sayHello = () =>  console.log("Hello World 2") ;
+
+const add = (a,b) => { return(a+b) }
+
+module.exports = {
+    sayHello,
+    add
+}
